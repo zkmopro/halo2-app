ee5a05fbc183b2fb9c69bb091b5203e99ae5a180
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-07-18 12:49:15 +0100

    (feat: iOS) iOS version working

---
+++ ios/mopro-test/mopro_Halo2App.swift
@@ -0,0 +1,17 @@
+//
+//  mopro_testApp.swift
+//  mopro-test
+//
+//  Created by Chance on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct mopro_Halo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
